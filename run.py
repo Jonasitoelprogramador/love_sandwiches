07e2a148201c19f073e925ce98c9eb26257719e4
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-06-01 14:09:40 +0000

    collect slaes data for last 5 markets

--- run.py
+++ run.py
@@ -75,6 +75,16 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully\n")
 
 
+def get_last_five_entries_sales():
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """Run all program functions"""
     data = get_sales_data()
